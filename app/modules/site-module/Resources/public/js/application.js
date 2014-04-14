129b7c2e55e860e2e9adbbebe43711d63c1d34cc
Josef Kříž <pepakriz@gmail.com>
2014-04-14 23:53:23 +0200

    Úprava barev a další drobnosti pro volby do EP

--- app/modules/site-module/Resources/public/js/application.js
+++ app/modules/site-module/Resources/public/js/application.js
@@ -44,30 +44,39 @@ $(document).ready(function() {
 	var toggleNavigation = function() {
 		var top = $(document).scrollTop();
 
-		if (top > 86 && top < 146) {
-			var t = top - 86;
+		if (top > 106 && top < 166) {
+			var t = top - 106;
 			t = t / 0.6 / 100 / 4;
 			$('.navigation-container').css('-webkit-box-shadow', '0px 0px 20px 0px rgba(50, 50, 50, ' + t + ')');
 			$('.navigation-container').css('-moz-box-shadow', '0px 0px 20px 0px rgba(50, 50, 50, ' + t + ')');
 			$('.navigation-container').css('box-shadow', '0px 0px 20px 0px rgba(50, 50, 50, ' + t + ')');
+			$('.navigation-container').css('background-color', 'rgba(255, 248, 248, ' + t + ')');
+			$('.navigation-container .navbar-brand').css('width', (t*4*195) + 'px');
+			$('.navigation-container .navbar-brand img').css('width', (t*100 * 4) + '%');
 			_s = false;
-		} else if (top <= 86 && _s != 1) {
+		} else if (top <= 106 && _s != 1) {
 			$('.navigation-container').css('-webkit-box-shadow', 'none');
 			$('.navigation-container').css('-moz-box-shadow', 'none');
 			$('.navigation-container').css('box-shadow', 'none');
+			$('.navigation-container').css('background-color', '#f8f8f8');
+			$('.navigation-container .navbar-brand').css('width', '0px');
+			$('.navigation-container .navbar-brand img').css('width', '0%');
 			_s = 1;
-		} else if (top >= 146 && _s != 2) {
+		} else if (top >= 166 && _s != 2) {
 			$('.navigation-container').css('-webkit-box-shadow', '0px 0px 20px 0px rgba(50, 50, 50, 0.25)');
 			$('.navigation-container').css('-moz-box-shadow', '0px 0px 20px 0px rgba(50, 50, 50, 0.25)');
 			$('.navigation-container').css('box-shadow', '0px 0px 20px 0px rgba(50, 50, 50, 0.25)');
+			$('.navigation-container').css('background-color', '#f8f8f8');
+			$('.navigation-container .navbar-brand').css('width', '195px');
+			$('.navigation-container .navbar-brand img').css('width', '100%');
 			_s = 2;
 		}
 
-		if (top > 81 && !_w) {
+		if (top > 101 && !_w) {
 			_w = true;
 			$('.navigation-container').removeClass('navbar-static-top');
 			$('.navigation-container').addClass('navbar-fixed-top');
-		} else if (top <= 81 && _w ) {
+		} else if (top <= 101 && _w ) {
 			_w = false;
 			$('.navigation-container').removeClass('navbar-fixed-top');
 			$('.navigation-container').addClass('navbar-static-top');
